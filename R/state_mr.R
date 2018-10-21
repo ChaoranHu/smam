a4acac22f1eb81b949d788ad5c16f57e23d77769
Chaoran Hu <huchaoran.stat@gmail.com>
2018-10-21 13:48:46 -0400

    add cutoff and rewrite prob ratio code for state MR

--- R/state_mr.R
+++ R/state_mr.R
@@ -1,5 +1,41 @@
 ### estimation of whole state path in moving-resting model
 
+predM <- function(p.m, p.r) { ## internal func. for ratio of prob: fb
+    length <- length(p.m)
+    result <- numeric(length)
+
+    for (i in 1:length) {
+        if (is.infinite(p.m[i]) & p.m[i] < 0) {
+            result[i] <- 0
+            next
+        }
+        if (is.infinite(p.r[i]) & p.r[i] < 0) {
+            result[i] <- 1
+            next
+        }
+        result[i] <- p.m[i] / (p.m[i] + p.r[i])
+    }
+    result
+}
+
+predMV <- function(p.m, p.r) { ## internal func. for ratio of prob: viterbi
+    length <- length(p.m)
+    result <- numeric(length)
+
+    for (i in 1:length) {
+        if (is.infinite(p.m[i]) & p.m[i] < 0) {
+            result[i] <- 0
+            next
+        }
+        if (is.infinite(p.r[i]) & p.r[i] < 0) {
+            result[i] <- 1
+            next
+        }
+        result[i] <- 1 / (1 + exp(p.r[i] - p.m[i]))
+    }
+    result
+}
+
 ##' Estimation of states at each time point with Moving-Resting Process
 ##'
 ##' Estimate the state at each time point under the Moving-Resting
@@ -27,6 +63,7 @@
 ##' time, and other columns are location coordinates.
 ##' @param theta the parameters for Moving-Resting model, in the
 ##' order of rate of moving, rate of resting, volatility.
+##' @param cutoff the cut-off point for prediction.
 ##' @param integrControl Integration control vector includes rel.tol,
 ##' abs.tol, and subdivisions.
 ##' @param startpoint Start time point of interested time interval.
@@ -51,13 +88,13 @@
 ##' set.seed(06269)
 ##' tgrid <- seq(0, 400, by = 8)
 ##' dat <- rMR(tgrid, 4, 3.8, 5, 'm')
-##' fitStateMR(dat, c(4, 3.8, 5))
-##' fitViterbiMR(dat, c(4, 3.8, 5))
-##' fitPartialViterbiMR(dat, c(4, 3.8, 5), 20, 10)
+##' fitStateMR(dat, c(4, 3.8, 5), cutoff = 0.5)
+##' fitViterbiMR(dat, c(4, 3.8, 5), cutoff = 0.5)
+##' fitPartialViterbiMR(dat, c(4, 3.8, 5), cutoff = 0.5, 20, 10)
 ##'
 ##' @author Chaoran Hu
 ##' @export
-fitStateMR <- function(data, theta,
+fitStateMR <- function(data, theta, cutoff = 0.5,
                        integrControl = integr.control()) {
     if (!is.matrix(data)) data <- as.matrix(data)
     dinc <- apply(data, 2, diff)
@@ -68,7 +105,8 @@ fitStateMR <- function(data, theta,
     cart_fwd <- cart_result[, 1:2]
     cart_bwd <- cart_result[, 3:4]
     cart_result <- cart_fwd * cart_bwd
-    cart_state <- 2 - apply(cart_result, 1, which.max)
+    predM <- predM(cart_result[, 1], cart_result[, 2])
+    cart_state <- ifelse(predM > cutoff, 1, 0)
     result <- cbind(data, cart_result, cart_state)
     colnames(result)[(ncol_data+1):(ncol_data+3)] <- c("p.m", "p.r", "states")
     as.data.frame(result)
@@ -77,7 +115,7 @@ fitStateMR <- function(data, theta,
 
 ##' @rdname fitStateMR
 ##' @export
-fitViterbiMR <- function(data, theta,
+fitViterbiMR <- function(data, theta, cutoff = 0.5,
                          integrControl = integr.control()) {
     if (!is.matrix(data)) data <- as.matrix(data)
     dinc <- apply(data, 2, diff)
@@ -85,7 +123,8 @@ fitViterbiMR <- function(data, theta,
     ncol_data <- ncol(data)
 
     cart_result <- viterbi_mr(theta, dinc, integrControl)
-    cart_state <- 2 - apply(cart_result, 1, which.max)
+    predM <- predMV(cart_result[, 1], cart_result[, 2])
+    cart_state <- ifelse(predM > cutoff, 1, 0)
     result <- cbind(data, cart_result, cart_state)
     colnames(result)[(ncol_data+1):(ncol_data+3)] <- c("p.m", "p.r", "states")
     as.data.frame(result)
@@ -93,7 +132,8 @@ fitViterbiMR <- function(data, theta,
 
 ##' @rdname fitStateMR
 ##' @export
-fitPartialViterbiMR <- function(data, theta, startpoint, pathlength,
+fitPartialViterbiMR <- function(data, theta, cutoff = 0.5,
+                                startpoint, pathlength,
                                 integrControl = integr.control()) {
     if (!is.matrix(data)) data <- as.matrix(data)
     nrow_data <- nrow(data)
@@ -104,7 +144,8 @@ fitPartialViterbiMR <- function(data, theta, startpoint, pathlength,
     ncol_data <- ncol(data)
 
     cart_result <- partial_viterbi_mr(theta, dinc, integrControl, startpoint - 1, pathlength)
-    cart_state <- 2 - apply(cart_result, 1, which.max)
+    predM <- predMV(cart_result[, 1], cart_result[, 2])
+    cart_state <- ifelse(predM > cutoff, 1, 0)
     result <- cbind(data[startpoint:(startpoint+pathlength-1), ], cart_result, cart_state)
     colnames(result)[(ncol_data+1):(ncol_data+3)] <- c("p.m", "p.r", "states")
     as.data.frame(result)
