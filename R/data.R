25d3fb8d025bb4a5ba943460c446e2c029b49e0c
Chaoran Hu <huchaoran.stat@gmail.com>
2020-10-02 16:51:11 -0400

    add f109 data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+##' GPS data of f109
+##'
+##' A dataset of GPS coordinates of a mature female mountain
+##' lion living in the Gros Ventre Mountain Range near
+##' Jackson, Wyoming. The data were collected by a code-only
+##' GPS wildlife tracking collar from 2019 to 2012.
+##'
+##' @format A data frame with 3917 rows and 3 variables:
+##' \describe{
+##'     \item{t}{time when the GPS coordinates were collected (unit: year)}
+##'     \item{dE}{UTM easting (unit: meter)}
+##'     \item{dN}{UTM northing (unit: meter)}
+##' }
+"f109"
